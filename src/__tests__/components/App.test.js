642b4857c1902b371132d5392948326882089d73
verissimohenry <verissimohenry04@gmail.com>
2021-09-24 16:18:37 +0100

    Fix linter error

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { cleanup, render, screen } from '@testing-library/react';

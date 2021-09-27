322a5f8ec2115f697f3a2dd30c00e9453433aff2
verissimohenry <verissimohenry04@gmail.com>
2021-09-27 12:26:33 +0100

    Fix App.test.js file

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -1,40 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import App from '../../components/App';
 import store from '../../store/store';
 import '@testing-library/jest-dom';
 
-const { expect } = require('chai');
-
 afterEach(cleanup);
-describe('App', () => {
-  it('should render the catalogue of recipe', () => {
-    const { asFragment } = render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>,
-    );
-    expect(
-      asFragment(
-        <Provider store={store}>
-          <App />
-        </Provider>,
-      ),
-    ).toMatchSnapshot();
-  });
-  it("Displays website's title", () => {
-    render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>,
-    );
-    const element = screen.getByText(/Catalogue of Recipe/i);
-    expect(element).toBeInTheDocument();
-  });
+
+it('should take a snapshot', () => {
+  const { asFragment } = render(<Provider store={store}><App /></Provider>);
+  expect(asFragment(<Provider store={store}><App /></Provider>)).toMatchSnapshot();
 });

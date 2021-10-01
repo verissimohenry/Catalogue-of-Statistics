30b63eb84f567165796002c2ecb05c59c9a049dd
verissimohenry <verissimohenry04@gmail.com>
2021-10-01 16:15:37 +0100

    fix linter error

--- src/__tests__/components/Recipe.test.js
+++ src/__tests__/components/Recipe.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import Recipe from '../../components/Recipe';
+import Recipe from '../../containers/Recipe';
 import store from '../../store/store';
 
 afterEach(cleanup);
